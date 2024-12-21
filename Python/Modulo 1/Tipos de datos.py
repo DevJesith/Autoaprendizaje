26b1d90b39cdf60b07c3be1e748fd03f38698dc2
Jesith Mejia <jesithmanuel@gmail.com>
2024-12-21 09:46:20 -0500

    Python

---
+++ Python/Modulo 1/Tipos de datos.py
@@ -0,0 +1,2 @@
+#Los tipos de datos pueden ser: Numericos (Integer, Float, numero complejo), secuencia (String, List, tuples), diccionario, Booleano y conjunto
+
---
+++ Python/Modulo 1/Variables.py
@@ -0,0 +1,37 @@
+#camelCase
+#myName
+# snake_case
+#my_name
+
+# x = 5
+
+# print(x)
+
+# x = "Hello"
+
+# print(x)
+
+#Asignar valor al mismo tiempo a varias variables
+# a = b = c = 10
+
+# print(a)
+# print(b)
+# print(c)
+
+# Asignar valores al mismo tiempo pero con diferentes datos
+
+# a,b,c = 8, 22, 30
+
+# print(a)
+# print(b)
+# print(c)
+
+# Como se elimina una variable
+
+a = 10
+
+print(a)
+
+del a 
+
+print(a)
