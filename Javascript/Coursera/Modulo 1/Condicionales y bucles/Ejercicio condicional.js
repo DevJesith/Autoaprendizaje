8fdcab8af94b3656cdcda855d93760e88b0bdf86
Jesith Mejia <jesithmanuel@gmail.com>
2024-12-30 16:43:29 -0500

    P

---
+++ Javascript/Coursera/Modulo 1/Condicionales y bucles/Ejercicio condicional.js
@@ -0,0 +1,44 @@
+//Complete los siguientes pasos para crear: ¿Tiene la edad suficiente?
+var age = 10;
+
+if (age >= 65) {
+    console.log("You get your income from your pension")
+} else if (age <= 65 && age >= 18) {
+    console.log("Each month you get a salary")
+} else if (age < 18) {
+    console.log("You get an allowance ")
+} else{
+    console.log("The value of the age variable is not numerical")
+}
+
+// Codifique el programa de los días de la semana como una declaración switch
+
+var day = "Sunday";
+
+switch (day) {
+    case "Monday":
+        console.log("Do something")
+        break;
+    case "Tuesday":
+        console.log("Do something")
+        break;
+    case "Wedsneday":
+        console.log("Do something")
+        break;
+    case "Thursday":
+        console.log("Do something")
+        break;
+    case "Friday":
+        console.log("Do something")
+        break;
+    case "Saturday":
+        console.log("Do something")
+        break;
+    case "Sunday":
+        console.log("Do something")
+        break;
+
+    default:
+        console.log("There is no such day")
+        break;
+}
