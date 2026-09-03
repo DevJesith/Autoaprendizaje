08737f246b3fdcd68e258dac68052b65ad396e0b
Jesith Mejia <jesithmanuel@gmail.com>
2026-09-03 12:09:19 -0500

    feat: initialize project

---
+++ SpringBoot/API-REST-Gestion-Tareas/src/main/java/com/portafolio/taskmanager/ApiRestGestionTareasApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestGestionTareasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestGestionTareasApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/API-REST-Gestion-Tareas/src/test/java/com/portafolio/taskmanager/ApiRestGestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestGestionTareasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
